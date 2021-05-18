9d06ece4a33d054d63544fd1ead1351b145df688
hoon <hoon@haechi.io>
2021-05-18 17:38:58 +0900

    audit contract

--- contracts/mocks/OwnableMock.sol
+++ contracts/mocks/OwnableMock.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.0;
+pragma solidity ^0.6.0;
 
-import "../library/Ownable.sol";
+import "../ATHLON.sol";
 
 contract OwnableMock is Ownable {
-
-    constructor() Ownable() {}
+    constructor() Ownable() public {}
 }
